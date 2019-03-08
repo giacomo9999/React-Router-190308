3312bb334af3bb4ef3d7233f2278535fc8f6b152
James Gary <gmarchesi888@gmail.com>
2019-03-08 11:53:02 -0500

    This is fascinating but I need to return to the task at hand.

--- src/components/users.component.js
+++ src/components/users.component.js
@@ -1,8 +1,38 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
+
+const userList = [
+  { name: "Wilhelmina", id: 1 },
+  { name: "Gertrude", id: 2 },
+  { name: "Rasputin", id: 3 }
+];
+
+const User = ({ location }) => <p>{location.pathname}</p>;
+// const User = () => <p>{"User Name"}</p>;
 
 class Users extends React.Component {
   render() {
-    return <h1>Users</h1>;
+    console.log(this.props);
+    const { url } = this.props.match;
+
+    return (
+      <div>
+        <h1>Users</h1>
+        <strong>Select A User</strong>
+        <div>
+          <ul>
+            {userList.map(entry => {
+              return (
+                <li key={entry.id}>
+                  <Link to={`/users/${entry.name}`}>{entry.name}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <Route path="/users/:id" component={User} />
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import { Route, Link, Switch, BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import Contact from "./components/contact.component";
 import Users from "./components/users.component";
+import Notfound from "./components/notFound.component";
 import * as serviceWorker from "./serviceWorker";
 
 const routing = (
   <Router>
     <div>
-      <Route exact path="/" component={App} />
-      <Route path="/users" component={Users} />
-      <Route path="/contact" component={Contact} />
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/users" component={Users} />
+        <Route path="/contact" component={Contact} />
+        <Route component={Notfound} />
+      </Switch>
     </div>
   </Router>
 );
